f8dd2ea86990a2bef98fc378556002aedcb62f6f
mr chapai <myicskt@gmail.com>
2023-01-19 11:16:15 -0600

    my first commit

---
+++ src/test/java/test/TC_02_ValidateRemoveItems_Test.java
@@ -0,0 +1,31 @@
+package test;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+import page.TC_01_ValidateCheckBox_Page;
+import page.TC_02_ValidateRemoveItems_Page;
+import util.BrowserFactory;
+
+public class TC_02_ValidateRemoveItems_Test {
+	// Test 2: Validate that a single list item could be removed using the remove
+	// button when a single checkbox is selected.
+
+	WebDriver driver;
+
+	@Test
+	public void validate_removeSingleList() throws IOException {
+		driver = BrowserFactory.init();
+		TC_02_ValidateRemoveItems_Page testCase02 = PageFactory.initElements(driver, TC_02_ValidateRemoveItems_Page.class);
+		testCase02.validate_removeSingleList();
+		BrowserFactory.tearDown();
+
+	}
+
+}
